481f4f925c1a6eb2224b5781777997bf53a88464
ikee <blazej.wodecki@icloud.com>
2022-11-05 02:10:59 +0100

    ❇️ node version downgrade, added node-fetch package

--- src/deploy-commands-dev.ts
+++ src/deploy-commands-dev.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import fs from "node:fs";
 import path from "path";
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 import fs from "fs";
 import path from "node:path";
