615366171b23b610928e5617630ef87918c849b0
ikee <blazej.wodecki@icloud.com>
2022-11-09 23:47:56 +0100

    🌐 refactoring taskmanager

---
+++ src/types/TaskTypes.ts
@@ -0,0 +1,31 @@
+import {
+  ButtonInteraction,
+  CommandInteraction,
+  ContextMenuCommandInteraction,
+  MessageComponentInteraction,
+} from "discord.js";
+import { FeedbackManager } from "../utils/managers/FeedbackManager";
+
+export interface TaskBase {
+  action: string;
+  interaction?:
+    | CommandInteraction
+    | ButtonInteraction
+    | ContextMenuCommandInteraction;
+  feedback?: FeedbackManager;
+  // emoteReference?: string;
+  message?: MessageComponentInteraction;
+  // storeId?: string;
+  // multiAdd?: boolean;
+  // options?: {
+  //   currentPage?: number;
+  //   pagesLimit?: number;
+  // };
+}
+
+export interface EmoteNavigatorTask extends TaskBase {
+  storeId: string;
+  multiAdd: boolean;
+  currentPage: number;
+  totalPages: number;
+}
