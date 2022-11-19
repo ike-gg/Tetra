364b1ccfa96500fcfb2ca6f4d2fe3dae13f0a5c6
ike <blazej@wodecki.dev>
2022-11-19 18:37:00 +0100

    ➕ added lost file

---
+++ src/emotes/addEmoteToGuild.ts
@@ -0,0 +1,21 @@
+import { Guild } from "discord.js";
+import { ExtractedEmote } from "../types";
+import { FeedbackManager } from "../utils/managers/FeedbackManager";
+
+const addEmoteToGuild = async (
+  emote: ExtractedEmote,
+  guild: Guild,
+  feedback: FeedbackManager
+) => {
+  try {
+    const addedEmote = await guild.emojis.create({
+      attachment: emote.finalData,
+      name: emote.name,
+    });
+    await feedback.successedAddedEmote(addedEmote);
+  } catch (error) {
+    throw new Error(String(error));
+  }
+};
+
+export default addEmoteToGuild;
