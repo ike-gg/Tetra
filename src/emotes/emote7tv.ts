d5c4305b88fa368efa58c4a939a6e69fc4240590
ike <blazej@wodecki.dev>
2023-02-17 03:24:51 +0100

    🛝 added feedback if api call failed

--- src/emotes/emote7tv.ts
+++ src/emotes/emote7tv.ts
@@ -8,60 +8,59 @@ import getRawEmote from "../api/7tv/getRawEmote";
 import emoteOptimise from "./emoteOptimise";
 
 import { ExtractedEmote } from "../types";
-const emote7tv = async (emoteReference: string, feedback?: FeedbackManager) => {
-  return new Promise<ExtractedEmote>(async (resolve, reject) => {
-    if (!emoteReference) throw new Error("Invalid emote reference.");
-    let internalId: string = emoteReference;
+const emote7tv = async (
+  emoteReference: string,
+  feedback?: FeedbackManager
+): Promise<ExtractedEmote> => {
+  if (!emoteReference) throw new Error("Invalid emote reference.");
+  let internalId: string = emoteReference;
 
-    if (isValidURL(internalId)) {
-      let fullURL = new URL(emoteReference);
-      let pathnamesArray = fullURL.pathname.split("/");
-      internalId = pathnamesArray.find((path) => path.length === 24)!;
-    }
+  if (isValidURL(internalId)) {
+    let fullURL = new URL(emoteReference);
+    let pathnamesArray = fullURL.pathname.split("/");
+    internalId = pathnamesArray.find((path) => path.length === 24)!;
+  }
 
-    if (!internalId) return;
+  if (!internalId) {
+    throw new Error("Invalid emote reference or URL");
+  }
 
-    if (internalId === undefined || internalId.length !== 24) {
-      reject("Invalid emote reference or URL");
-      return;
-    }
+  if (internalId === undefined || internalId.length !== 24) {
+    throw new Error("Invalid emote reference or URL");
+  }
 
-    try {
-      const emoteInfo = (await getEmoteInfo(internalId)) as EmoteResponseAPI;
+  try {
+    const emoteInfo = (await getEmoteInfo(internalId)) as EmoteResponseAPI;
 
-      if (emoteInfo.error) reject(`Emote not found. ${emoteInfo.error}`);
+    if (emoteInfo.error) throw new Error(`Emote not found. ${emoteInfo.error}`);
 
-      let emotePreview = `https:${emoteInfo.host.url}/2x`;
-      emoteInfo.animated ? (emotePreview += ".gif") : (emotePreview += ".webp");
+    let emotePreview = `https:${emoteInfo.host.url}/2x`;
+    emoteInfo.animated ? (emotePreview += ".gif") : (emotePreview += ".webp");
 
-      const rawEmote = await getRawEmote(
-        emoteInfo.host.url,
-        emoteInfo.animated
-      );
-      const rawEmoteBuffer = Buffer.from(rawEmote!);
+    const rawEmote = await getRawEmote(emoteInfo.host.url, emoteInfo.animated);
+    const rawEmoteBuffer = Buffer.from(rawEmote!);
 
-      const emoteBuffer = await emoteOptimise(rawEmoteBuffer, {
-        animated: emoteInfo.animated,
-        feedback: feedback,
-      });
+    const emoteBuffer = await emoteOptimise(rawEmoteBuffer, {
+      animated: emoteInfo.animated,
+      feedback: feedback,
+    });
 
-      let authorNickname = emoteInfo.owner?.display_name;
-      if (!authorNickname) authorNickname = "DeletedUser";
+    let authorNickname = emoteInfo.owner?.display_name;
+    if (!authorNickname) authorNickname = "DeletedUser";
 
-      resolve({
-        author: authorNickname,
-        name: emoteInfo.name,
-        data: rawEmoteBuffer,
-        finalData: emoteBuffer,
-        preview: emotePreview,
-        animated: emoteInfo.animated,
-        origin: "7tv",
-        id: emoteReference,
-      });
-    } catch (error) {
-      reject(String(error));
-    }
-  });
+    return {
+      author: authorNickname,
+      name: emoteInfo.name,
+      data: rawEmoteBuffer,
+      finalData: emoteBuffer,
+      preview: emotePreview,
+      animated: emoteInfo.animated,
+      origin: "7tv",
+      id: emoteReference,
+    };
+  } catch (error) {
+    throw new Error(String(error));
+  }
 };
 
 export default emote7tv;
