96bce16eafc9432a535dc810679fcd7e937f5e8c
ike <blazej@wodecki.dev>
2023-02-25 23:50:50 +0100

    🔧 replaced functionality with addsubemote

---
+++ src/commands/addSubEmote.ts
@@ -0,0 +1,51 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+
+import { FeedbackManager } from "../utils/managers/FeedbackManager";
+import { DiscordBot } from "../types";
+import addSubEmoteChannel from "../subcommands/addSubEmoteChannel";
+
+const importEmote = {
+  data: new SlashCommandBuilder()
+    .setName("addsubemote")
+    .setDescription("Add sub emote")
+    //subcommand byname
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("bychannel")
+        .setDescription("Use Twitch Channel to fetch emotes from it.")
+        .addStringOption((option) =>
+          option
+            .setName("channelname")
+            .setDescription("Name of the twitch channel")
+            .setRequired(true)
+        )
+    ),
+  async execute(interaction: ChatInputCommandInteraction, client: DiscordBot) {
+    // const ephemeral = !interaction.memberPermissions!.has(
+    //   "ManageEmojisAndStickers"
+    // );
+    // const feedback = new FeedbackManager(interaction, { ephemeral });
+
+    const feedback = new FeedbackManager(interaction);
+
+    if (!interaction.memberPermissions?.has("ManageEmojisAndStickers")) {
+      await feedback.missingPermissions();
+      return;
+    }
+
+    await feedback.gotRequest();
+
+    const subcommandUsed = interaction.options.getSubcommand();
+
+    switch (subcommandUsed) {
+      case "bychannel":
+        addSubEmoteChannel(interaction, client, feedback);
+        break;
+      default:
+        feedback.error("Subcommand not supported yet.");
+        break;
+    }
+  },
+};
+
+export default importEmote;
