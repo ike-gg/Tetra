cbe77844530863cb59f74591e1e0f111a002d6c5
ike-gg <blazej.wodecki@icloud.com>
2023-04-02 17:44:03 +0200

    feat: log of adding emote using manual adjustment

--- src/api/tasks/postTask.ts
+++ src/api/tasks/postTask.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { client } from "../../index";
 import TaskManager from "../../utils/managers/TaskManager";
+import { TextChannel } from "discord.js";
 
 export default async (req: Request, res: Response) => {
   const { guildId, emote, name, taskId } = req.body;
@@ -29,6 +30,24 @@ export default async (req: Request, res: Response) => {
       ?.emojis.create({ attachment: emoteBuffer, name: name });
 
     tasks.removeTask(taskId);
+
+    try {
+      const announceChannel = (await client.channels.fetch(
+        "1054273914437648384"
+      )) as TextChannel;
+
+      if (!announceChannel) return;
+
+      await announceChannel.send(
+        `> Someone just added an emote ${emote} to their server using **Manual Adjustment**! ${
+          Math.random() > 0.8
+            ? `\nTry to use \`steal\` command on this message to add emote to your server!`
+            : ""
+        }`
+      );
+    } catch (error) {
+      console.error("Cant reach announcement channel");
+    }
 
     res.status(200).json({
       message: "Emote created",
