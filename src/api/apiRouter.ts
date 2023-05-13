9ffd94d86253f338ed204604dd3b8920af8737b5
ike <67127723+ike-gg@users.noreply.github.com>
2023-05-13 21:35:26 +0200

    feat: joined auth route to api

--- src/api/apiRouter.ts
+++ src/api/apiRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import taskRouter from "./tasks/taskRouter";
 import bufferRouter from "./buffer/bufferRouter";
+import authRouter from "./auth/authRouter";
 
 const apiRouter = Router();
 
 apiRouter.use("/task", taskRouter);
 apiRouter.use("/buffer", bufferRouter);
+apiRouter.use("/auth", authRouter);
 
 export default apiRouter;
