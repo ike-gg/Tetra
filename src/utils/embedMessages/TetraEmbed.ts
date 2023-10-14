4cf79aa58c8252610fb1c47ec52fb06e42a1f3ac
ike <67127723+ike-gg@users.noreply.github.com>
2023-10-14 23:54:08 +0200

    feat: new tetraembed class for unified embeds

---
+++ src/utils/embedMessages/TetraEmbed.ts
@@ -0,0 +1,72 @@
+import { EmbedBuilder, EmbedData } from "discord.js";
+
+export type TetraEmbedContent = EmbedData | string;
+
+const getEmojiTitle = (emoji: String, title: string) => `${emoji}  ${title}`;
+
+export class TetraEmbed {
+  static __transform(content: TetraEmbedContent): EmbedData {
+    if (typeof content === "string") {
+      return { description: content };
+    }
+    return content;
+  }
+
+  static success(content: TetraEmbedContent) {
+    const details = this.__transform(content);
+    const embedTitle = getEmojiTitle("✅", details.title || "Success");
+
+    return new EmbedBuilder({
+      color: 0x3acf38,
+      ...details,
+      title: embedTitle,
+    });
+  }
+
+  static error(content: TetraEmbedContent) {
+    const details = this.__transform(content);
+    const embedTitle = getEmojiTitle(
+      "❌",
+      details.title || "Something went wrong"
+    );
+
+    return new EmbedBuilder({
+      color: 0xeb3434,
+      ...details,
+      title: embedTitle,
+    });
+  }
+
+  static attention(content: TetraEmbedContent) {
+    const details = this.__transform(content);
+    const embedTitle = getEmojiTitle("🟧", details.title || "Attention");
+
+    return new EmbedBuilder({
+      color: 0xff8812,
+      ...details,
+      title: embedTitle,
+    });
+  }
+
+  static warning(content: TetraEmbedContent) {
+    const details = this.__transform(content);
+    const embedTitle = getEmojiTitle("⚠️", details.title || "Warning");
+
+    return new EmbedBuilder({
+      color: 0xf7e139,
+      ...details,
+      title: embedTitle,
+    });
+  }
+
+  static info(content: TetraEmbedContent) {
+    const details = this.__transform(content);
+    const embedTitle = getEmojiTitle("ℹ️", details.title || "Information");
+
+    return new EmbedBuilder({
+      color: 0xf0f0f0,
+      ...details,
+      title: embedTitle,
+    });
+  }
+}
