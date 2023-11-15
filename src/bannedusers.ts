80c79c4ced0b58d29539b16be6c3ae00f8010789
ike <67127723+ike-gg@users.noreply.github.com>
2023-11-15 07:13:57 +0100

    chore: free deku

--- src/bannedusers.ts
+++ src/bannedusers.ts
@@ -5,9 +5,4 @@ interface BanDetails {
 
 export const BANNEDLIST: BanDetails[] = [
   { userId: "388315180854935552", reason: "idiota" },
-  {
-    userId: "284815589241913346",
-    reason:
-      "Dostęp został ograniczony, zauważyliśmy podejrzaną aktywność i dla bezpieczeństwa ograniczyliśmy twój dostęp",
-  },
 ];
