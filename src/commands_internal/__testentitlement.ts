dc1b6697dae37cea0defe53bc3d0636790f5446b
ike <67127723+ike-gg@users.noreply.github.com>
2023-12-28 14:36:52 +0100

    chore: structure of internal commands

---
+++ src/commands_internal/__testentitlement.ts
@@ -0,0 +1,61 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { FeedbackManager } from "../utils/managers/FeedbackManager";
+
+const here = {
+  data: new SlashCommandBuilder()
+    .setName("testentitlement")
+    .setDescription("internal tetra tooling for testing entitlements")
+    .addStringOption((option) =>
+      option
+        .setName("guildid")
+        .setDescription(
+          "id of guild to enable test entitlement on it, if not provided, test will be applied on current guild"
+        )
+        .setRequired(false)
+    ),
+  async execute(interaction: ChatInputCommandInteraction) {
+    const feedback = new FeedbackManager(interaction);
+    try {
+      const guildId =
+        interaction.options.getString("guildid") ?? interaction.guildId;
+
+      if (!guildId) {
+        await feedback.error("No guild id provided");
+        return;
+      }
+
+      const skus = await interaction.client.application.fetchSKUs();
+
+      console.log("skus", skus);
+
+      const sku = skus.find((sku) => sku.type === 5);
+
+      if (!sku) {
+        await feedback.error("No premium sku found");
+        return;
+      }
+
+      console.log("creating test entitlement from:", sku);
+
+      const testEntitlement =
+        await interaction.client.application.entitlements.createTest({
+          guild: guildId,
+          sku: sku.id,
+        });
+
+      console.log("Created entitlement:", testEntitlement);
+
+      await feedback.success(
+        `Test entitlement created successfully, more details in the console.`
+      );
+    } catch (error) {
+      if (error instanceof Error) {
+        await feedback.error(error.message);
+      } else {
+        await feedback.unhandledError(error);
+      }
+    }
+  },
+};
+
+export default here;
