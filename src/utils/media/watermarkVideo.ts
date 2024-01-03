c487fd6c305bb48286bdc2a5ca15ab0760a7c2ec
ike <67127723+ike-gg@users.noreply.github.com>
2024-01-03 19:51:16 +0100

    feat: fn to watermarking videos

--- src/utils/media/watermarkVideo.ts
+++ src/utils/media/watermarkVideo.ts
@@ -1,52 +1,94 @@
-import { bundle } from "@remotion/bundler";
-//UNINSTALL BUNDLE.
-import { renderMedia, selectComposition } from "@remotion/renderer";
+import ffmpeg from "ffmpeg";
+import * as fs from "fs";
+import { tetraTempDirectory } from "../../constants";
 import path from "path";
-//@ts-ignore
-import ffprobe from "ffprobe-client";
+import { randomUUID } from "crypto";
 
-interface WatermarkVideoProps {
-  mediaUrl: string;
-  durationInFrames: number;
-  width: number;
-  height: number;
-}
+export const watermarkVideo = async (
+  video: Buffer,
+  id: string
+): Promise<Buffer> => {
+  return new Promise((resolve, reject) => {
+    const pid = randomUUID();
+    const tempDir = tetraTempDirectory(id);
+    const videoPath = `${tempDir}/in${pid}.mp4`;
+    const newVideoPath = `${tempDir}/out${pid}.mp4`;
 
-const compositionId = "tetramedia";
+    const watermarkPath = path.resolve(__dirname, "./watermark.png");
 
-export const watermarkVideo = async (url: string) => {
-  console.log("begin watermarking...");
-  // const bundleLocation = await bundle({
-  //   entryPoint: path.resolve("./watermark/bundle.js")
-  // })
+    fs.writeFileSync(videoPath, video);
 
-  console.log("ffprobe start");
-  const { format } = await ffprobe(url);
-  console.log("ffprobe end");
-
-  const { duration } = format;
-
-  const bundleLocation = path.resolve(__dirname, "./watermark");
-
-  console.log(bundleLocation);
-
-  const inputProps = {
-    mediaUrl: url,
-    durationInFrames: Math.ceil(duration * 30),
-  };
-
-  const composition = await selectComposition({
-    serveUrl: bundleLocation,
-    id: compositionId,
-    inputProps,
+    try {
+      new ffmpeg(videoPath, (err, video) => {
+        if (!err) {
+          video.fnAddWatermark(
+            watermarkPath,
+            newVideoPath,
+            {
+              position: "C",
+            },
+            (error, file) => {
+              if (!error) {
+                const outputBuffer = fs.readFileSync(file);
+                resolve(outputBuffer);
+              } else {
+                console.log(error);
+                reject(new Error("Failed to watermark video"));
+              }
+            }
+          );
+        } else {
+          reject(new Error("Failed to read video?"));
+        }
+      });
+    } catch (error) {
+      reject(new Error("FFMPEG failed."));
+    }
   });
-
-  const { buffer } = await renderMedia({
-    composition,
-    serveUrl: bundleLocation,
-    codec: "h264",
-    inputProps,
-  });
-
-  return buffer;
 };
+
+// remotion approach but it renders really slow.
+
+// import { renderMedia, selectComposition } from "@remotion/renderer";
+// import path from "path";
+// //@ts-ignore
+// import ffprobe from "ffprobe-client";
+
+// interface WatermarkVideoProps {
+//   mediaUrl?: string;
+//   durationInFrames?: number;
+//   width?: number;
+//   height?: number;
+// }
+
+// const compositionId = "tetramedia";
+// export const watermarkVideo = async (url: string) => {
+// const { format } = await ffprobe(url);
+
+//   const { duration } = format;
+
+//   const bundleLocation = path.resolve(__dirname, "./watermark");
+
+//   const inputProps: WatermarkVideoProps = {
+//     mediaUrl: url,
+//     durationInFrames: Math.ceil(duration * 30),
+//   };
+
+//   const composition = await selectComposition({
+//     serveUrl: bundleLocation,
+//     id: compositionId,
+//     //@ts-ignore
+//     inputProps,
+//   });
+
+//   const { buffer } = await renderMedia({
+//     composition,
+//     serveUrl: bundleLocation,
+//     codec: "h264",
+//     //@ts-ignore
+//     inputProps,
+//     onProgress: (p) => console.log(p),
+//   });
+
+//   return buffer;
+// };
