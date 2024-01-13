3fab41f3553e6a45dd2836760d2119d86fc9e7f5
ike <67127723+ike-gg@users.noreply.github.com>
2024-01-13 07:15:03 +0100

    fix: twitter working again

--- src/utils/media/handleTwitterMedia.ts
+++ src/utils/media/handleTwitterMedia.ts
@@ -1,69 +1,66 @@
+import fetch from "node-fetch";
 import { MediaOutput, PlatformResult } from "../../commands/media";
 import isValidURL from "../isValidURL";
 import { FeedbackManager } from "../managers/FeedbackManager";
+import * as z from "zod";
 
-// interface OutputTwitterFn extends Output {
-//   error?: String;
-// }
-
-// type TwitterFn = (
-//   url: string | MediaOptionsWithUrl,
-//   options?: MediaOptions
-// ) => Promise<OutputTwitterFn>;
-
-// const getTwitterMedia: TwitterFn = require("get-twitter-media");
-
-//@ts-ignore
-import { ndown } from "nayan-media-downloader";
+const apiResponseSchema = z.object({
+  mediaURLs: z.array(z.string().url()),
+  text: z.string(),
+});
 
 export const handleTwitterMedia = async (
   _url: string,
   feedback: FeedbackManager
 ): Promise<PlatformResult> => {
   try {
-    const urlVideo = _url
-      .replace("x.com", "twitter.com")
-      .replace("fxtwitter.com", "twitter.com");
+    const { pathname } = new URL(_url);
+
+    const apiUrl = new URL(pathname, "https://api.vxtwitter.com/").href;
+
+    const request = await fetch(apiUrl);
+
+    if (!request.ok) {
+      throw new Error("Request to API failed.");
+    }
+
+    const tweetData = await request.json();
+
+    const { mediaURLs, text } = apiResponseSchema.parse(tweetData);
+
+    const parsedMedia: MediaOutput[] = await Promise.all(
+      mediaURLs.map(async (item): Promise<MediaOutput> => {
+        const request = await fetch(item, { method: "HEAD" });
+        const headers = request.headers;
+
+        const contentType = headers.get("content-type");
+        const contentSize = headers.get("content-length");
+
+        const isVideo = contentType?.includes("video");
+        return {
+          source: item,
+          type: isVideo ? "mp4" : "jpg",
+          size: Number(contentSize),
+        };
+      })
+    );
+
+    const description =
+      text
+        ?.split(" ")
+        .slice(0, -1)
+        .filter((block) => !isValidURL(block))
+        .join(" ") || "-";
 
     return {
-      description: "Twitter temporarily disabled",
-      media: [],
-    };
-
-    // if ("error" in twitterData) {
-    //   console.log(twitterData);
-    //   return { description: "", media: [] };
-    // }
-
-    // const parsedMedia: MediaOutput[] = await Promise.all(
-    //   media.map(async (item): Promise<MediaOutput> => {
-    //     const request = await fetch(item.url, { method: "HEAD" });
-    //     const headers = request.headers;
-
-    //     const contentType = headers.get("content-type");
-    //     const contentSize = headers.get("content-length");
-
-    //     const isVideo = contentType?.includes("video");
-    //     return {
-    //       source: item.url,
-    //       type: isVideo ? "mp4" : "jpg",
-    //       size: Number(contentSize),
-    //     };
-    //   })
-    // );
-
-    // const description =
-    //   text
-    //     ?.split(" ")
-    //     .slice(0, -1)
-    //     .filter((block) => !isValidURL(block))
-    //     .join(" ") || "-";
-
-    return {
-      description: "",
-      media: [],
+      description: description,
+      media: parsedMedia,
     };
   } catch (error) {
-    throw error;
+    if (error instanceof z.ZodError) {
+      throw new Error("Received wrong response from API.");
+    } else {
+      throw error;
+    }
   }
 };
