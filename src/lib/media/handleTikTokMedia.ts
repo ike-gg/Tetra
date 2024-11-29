746665358f649ce53180ffe536bf827e5c77bf0b
ike-gg <67127723+ike-gg@users.noreply.github.com>
2024-11-29 22:01:49 +0100

    feat: tiktok support

--- src/lib/media/handleTikTokMedia.ts
+++ src/lib/media/handleTikTokMedia.ts
@@ -1,77 +1,53 @@
 import { PlatformHandlerCallback, PlatformResult } from "../../commands/media";
-import { Downloader } from "@tobyg74/tiktok-api-dl";
 import { EmbeddedError } from "../../constants/errors";
 import { getRemoteResourceDetails } from "../../utils";
+import { fetchCobaltAPI } from "./helper/fetchCobaltAPI";
 
 export const handleTikTokMedia: PlatformHandlerCallback = async (url) => {
-  const response = await Downloader(url, {
-    version: "v1",
-  });
+  const post = await fetchCobaltAPI(url);
 
-  const post = response.result;
-
-  if (response.status === "error" || !post) {
-    throw new EmbeddedError(response.message ?? "TikTok not found or link is invalid.");
+  if (post.status === "error" || !post) {
+    throw new EmbeddedError(
+      "TikTok not found or link is invalid. (Cobalt response with error status)"
+    );
   }
 
-  if (!post) {
-    throw new EmbeddedError("TikTok not found or link is invalid. (Empty result)");
+  // tiktok slideshow
+  if (post.status === "picker" && post.picker) {
+    const images = await Promise.all(
+      post.picker.map(async (item): Promise<PlatformResult["media"][0]> => {
+        const resource = await getRemoteResourceDetails(item.url);
+
+        return {
+          size: resource?.size,
+          type: "jpg",
+          source: item.url,
+        };
+      })
+    );
+
+    const audioUrl = post.audio;
+
+    return {
+      media: images,
+      isSlideshow: true,
+      audio: audioUrl,
+    };
   }
 
-  const { description, createTime, author, statistics } = post;
+  if (post.url) {
+    const { url } = post;
 
-  if (post.type === "video" && post.video) {
-    const videoUrl = post.video.downloadAddr.at(0);
-
-    if (!videoUrl) throw new EmbeddedError("TikTok video not found or link is invalid");
-
-    const resource = await getRemoteResourceDetails(videoUrl);
+    const resource = await getRemoteResourceDetails(url);
 
     return {
       media: [
         {
           size: resource?.size,
           type: "mp4",
-          source: videoUrl,
+          source: url,
         },
       ],
-      description,
-      metadata: {
-        author: author.nickname,
-        date: new Date(createTime * 1000),
-        views: statistics.playCount,
-        likes: statistics.diggCount,
-      },
-    };
-  }
-
-  // tiktok slideshow
-  if (post.type === "image" && post.images) {
-    const images = await Promise.all(
-      post.images.map(async (image): Promise<PlatformResult["media"][0]> => {
-        const resource = await getRemoteResourceDetails(image);
-
-        return {
-          size: resource?.size,
-          type: "jpg",
-          source: image,
-        };
-      })
-    );
-
-    const audioUrl = post.music.playUrl.at(0);
-
-    return {
-      media: images,
-      isSlideshow: true,
-      metadata: {
-        author: author.nickname,
-        date: new Date(createTime * 1000),
-        views: statistics.playCount,
-        likes: statistics.diggCount,
-      },
-      description,
-      audio: audioUrl,
     };
   }
 
